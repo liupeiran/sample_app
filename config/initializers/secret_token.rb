c5c7a7bdf3d3fd4e12babf140ddd3cda22bace9b
Peiran Liu <peiran.liu@lightspeedretail.com>
2013-12-28 17:31:28 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '36d72e89156427dd72b6fd3e5ac64b29d1d664a489c40431266c8badf41eb91b0db6404093c7a439e76c06cefd424e82fa03687127fabb88d5aec83a1adbec9e'
